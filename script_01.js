0114eb8431f47ef43b0d2ffb8f0fa2639bc23b47
Igor <igor.lischewski@web.de>
2021-02-15 15:38:50 +0100

    002/Objekt_Hierarchie

--- script_01.js
+++ script_01.js
@@ -16,32 +16,53 @@ function ausgabe(outputStr) {
 
 /***** Objekte 1 Daten/Funktionen *******/
 
-let person = {
-                firstName:"Drazen", 
-                familyName:"Savi",
-                salary: [120000,125000],
-                hasAccess: true,
-                sayHello: function(){
-                                        return "hi";
+// let person = {
+//                 firstName:"Drazen", 
+//                 familyName:"Savi",
+//                 salary: [120000,125000],
+//                 hasAccess: true,
+//                 sayHello: 
+//                 function() {
+//                                 return "Ich bin " + this.firstName;
+//                             }
+//             };
+
+
+
+// // ausgabe(person);
+// // ausgabe(person.firstName);
+// // ausgabe(person.familyName);
+// // ausgabe(person.salary[0]);
+// // ausgabe(person.hasAccess);
+
+// // let txt = "Ich bin " + person.firstName + " " +
+// //             person.familyName + "und verdiene " +
+// //             person.salary[1] + " p.a."
+
+// // ausgabe(txt);
+
+// // if (person.hasAccess) {
+// //     ausgabe("Sie sind ein VIP!");
+// // }
+
+// ausgabe(person.sayHello());
+
+
+/***** Objekte 2 - Hierarchie *******/
+
+// Tiefer, tiefer - Irgendwo ist der Tiefe gibt es ein Licht // K.Bush
+// Der Baikalsee ist mit 1642 m der tiefste See der Erde. ....
+// deep - deeper - deepest
+// Nested Objekt
+
+let baikal = {
+                deep:   {
+                            deeper: {
+                                      deepest: "Das Licht - auf 1642m."
                                     }
-            };
+                        }
+              }
 
-
-
-ausgabe(person);
-ausgabe(person.firstName);
-ausgabe(person.familyName);
-ausgabe(person.salary[0]);
-ausgabe(person.hasAccess);
-
-let txt = "Ich bin " + person.firstName + " " +
-            person.familyName + "und verdiene " +
-            person.salary[1] + " p.a."
-
-ausgabe(txt);
-
-if (person.hasAccess) {
-    ausgabe("Sie sind ein VIP!");
-}
-
-ausgabe(person.sayHello());
+ausgabe(baikal);
+// {{{{{ ... }}}}}  Nested Objects
+ausgabe(baikal.deep.deeper.deepest);
