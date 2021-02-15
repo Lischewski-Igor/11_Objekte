62a9ec4c3e1ce8711c62c0925574ac188872984f
Igor <igor.lischewski@web.de>
2021-02-15 12:37:55 +0100

    01/Objekt_mit_properties_&_methods

--- script_01.js
+++ script_01.js
@@ -9,3 +9,39 @@ function ausgabe(outputStr) {
 // arr = ["Ich","bin","Drazen"];
 // ausgabe(arr[2]);
 
+// arr = [["Ich", "bin", "Drazen"],["Ich", "bin", "Jenny"]];
+// ausgabe(arr[0][2]);
+// ausgabe(arr[1][2]);
+
+
+/***** Objekte 1 Daten/Funktionen *******/
+
+let person = {
+                firstName:"Drazen", 
+                familyName:"Savi",
+                salary: [120000,125000],
+                hasAccess: true,
+                sayHello: function(){
+                                        return "hi";
+                                    }
+            };
+
+
+
+ausgabe(person);
+ausgabe(person.firstName);
+ausgabe(person.familyName);
+ausgabe(person.salary[0]);
+ausgabe(person.hasAccess);
+
+let txt = "Ich bin " + person.firstName + " " +
+            person.familyName + "und verdiene " +
+            person.salary[1] + " p.a."
+
+ausgabe(txt);
+
+if (person.hasAccess) {
+    ausgabe("Sie sind ein VIP!");
+}
+
+ausgabe(person.sayHello());
